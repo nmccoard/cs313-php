a45750a051410c0857d8b77254a42f49882858bb
Nate M <64175378+nmccoard@users.noreply.github.com>
2020-05-15 22:22:28 -0400

    Added some basic insertion the location and course tables

--- db/myDb.sql
+++ db/myDb.sql
@@ -21,9 +21,9 @@ CREATE TABLE location(
 
 CREATE TABLE course (
     id SERIAL PRIMARY KEY,
-    description VARCHAR(128) NOT NULL,
+    equipment_type VARCHAR(64) NOT NULL,
     delivery_type VARCHAR(32) NOT NULL,
-    equipment_type VARCHAR(64) NOT NULL
+    description VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE student (
@@ -52,4 +52,20 @@ CREATE TABLE equipment(
 
 
 -- initial values
+
+INSERT INTO location (state) VALUES ('UT');
+INSERT INTO location (state) VALUES ('OH');
+INSERT INTO location (state) VALUES ('CA');
+INSERT INTO location (state) VALUES ('AZ');
+INSERT INTO location (state) VALUES ('ID');
+INSERT INTO location (state) VALUES ('TX');
+
+INSERT INTO course (description, delivery_type, equipment_type) VALUES ('10 day Steam Sterilizer course covering Eagle 3000 to AMSCO 600','Instructor Lead','Steam Sterilizer');
+INSERT INTO course (description, delivery_type, equipment_type) VALUES ('3 Online modules combined with 3 days of ILT covering the entire VPro Series of Sterilizers','Blended','VPro');
+INSERT INTO course (description, delivery_type, equipment_type) VALUES ('6 days Single Chamber Washer Training covering from 444 to 7053hp','Instructor Lead','Single Chamber Washer');
+INSERT INTO course (description, delivery_type, equipment_type) VALUES ('5 Online modules covering Surgical Tables from 3080 to 5085 ','Online','Surgical Tables');
+INSERT INTO course (description, delivery_type, equipment_type) VALUES ('4 Online modules covering Surgical Light from LC to M-series','Online','Surgical Lights');
+INSERT INTO course (description, delivery_type, equipment_type) VALUES ('4 day 130 and 1300 Series Cart Washer Training','Instructor Lead','Cart Washer');
+INSERT INTO course (description, delivery_type, equipment_type) VALUES ('4 Online modules and 5 days of ILT to cover iQ and IDSS OR integration systems ','Blended','OR Integration');
+
 
